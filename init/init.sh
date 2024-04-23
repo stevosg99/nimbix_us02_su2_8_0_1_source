91cb4e80a0faec66410827c2fbd961b00d5ebeca
stevosg99 <96193330+stevosg99@users.noreply.github.com>
2024-04-23 14:17:17 -0500

    Update init.sh

--- init/init.sh
+++ init/init.sh
@@ -4,14 +4,14 @@ sleep 10
 
 # Compile SU2 on the main node in the session
 echo "Compiling SU2"
-/usr/local/SU2/init/compile_SU2.sh
+/tmp/SU2/init/compile_SU2.sh
 
 echo "Changing to /data/SU2 directory to begin data processing."
 
 cd /data/SU2
 
 # Provide permission to run bash file in /data directory
-sudo chmod -R 0777 /data/SU2
+chmod -R 0777 /data/SU2
 
 # Get bash filename from session initialization
 while [[ -n "$1" ]]; do
